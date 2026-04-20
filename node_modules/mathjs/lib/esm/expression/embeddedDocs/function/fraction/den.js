b47e305bbe1e5b4c8612b2d36d102290f62ace76
yuutokouno <rarara.yuto.0510@gmail.com>
2026-04-20 17:31:52 +0900

    Revert "gitignoreから"
    This reverts commit ffe989769ad66da2505a86e4bbe76c2ffe4cddec.

--- node_modules/mathjs/lib/esm/expression/embeddedDocs/function/fraction/den.js
+++
@@ -1,8 +0,0 @@
-export var denDocs = {
-  name: 'den',
-  category: 'Fraction',
-  syntax: ['den(x)'],
-  description: 'Get the denominator of a fraction.',
-  examples: ['den(fraction(2, 3))', 'den(fraction(5, 8))'],
-  seealso: ['num', 'fraction']
-};
